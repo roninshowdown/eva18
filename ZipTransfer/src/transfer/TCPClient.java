d351cc0a1d03916462743edd72932ea662d92a67
DP1997 <donaldp78@googlemail.com>
2018-12-12 09:54:20 +0100

    test

--- ZipTransfer/src/transfer/TCPClient.java
+++ ZipTransfer/src/transfer/TCPClient.java
@@ -1,92 +1,53 @@
 package transfer;
 
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.Socket;
-import java.util.zip.ZipFile;
+import java.io.*;
+import java.io.ByteArrayOutputStream;
+import java.net.*;
 
-public class TCPClient {
-    public final static int SOCKET_PORT = 13267;      
-    public final static String SERVER = "00.200.00.00";  
-    public final static String
-    FILE_TO_RECEIVED = "D:/Projects/Transferred.zip";
-    public final static int FILE_SIZE = 5830740;
+class TCPClient {
+
+    private final static String serverIP = "127.0.0.1";
+    private final static int serverPort = 3248;
+    private final static String fileOutput = "C:\\testout.pdf";
+
     public static void main(String args[]) {
-        int bytesRead;
-        int current = 0;
-        PrintWriter pwr=null;
-        FileOutputStream fos = null;
-        BufferedOutputStream bos = null;
-        BufferedReader br = null;
-        Socket sock = null;
-        String filesize= null, s=null;
+        byte[] aByte = new byte[1];
+        int bytesRead = -1;
+
+        Socket clientSocket = null;
+        InputStream is = null;
 
         try {
-            sock = connectToServer(SERVER, SOCKET_PORT);
-            System.out.println("Connecting...");
-            br= new BufferedReader(new InputStreamReader(sock.getInputStream()));
-            pwr = new PrintWriter(sock.getOutputStream());
-            String input=null, output = "SENDZIP";
-            while((input = br.readLine()) != null){
-                System.out.println("INPUT is "+input);
-                if (input.contains("SENTZIP")){
-                    byte [] mybytearray  = new byte [Integer.parseInt(s)];
-                    InputStream is = sock.getInputStream();
-                    fos = new FileOutputStream(FILE_TO_RECEIVED);
-                    bos = new BufferedOutputStream(fos);
-                    bytesRead = is.read(mybytearray,0,mybytearray.length);
-                    current = bytesRead;
-                    do {
-                        bytesRead = is.read(mybytearray, current, (mybytearray.length-current));
-                        if(bytesRead >= 0) current += bytesRead;
-                    } while(current < FILE_SIZE);
-                    bos.write(mybytearray, 0 , current);
-                    bos.flush();
-                    ZipFile file = new ZipFile(FILE_TO_RECEIVED) ; 
-                    System.out.println(file.size()+ " zip files are received in  the client"); 
-                    output = "Received"+file.size();
-                    System.out.println(input+" when output is "+output);
-                    pwr.println(output);
-                }
+            clientSocket = new Socket( serverIP , serverPort );
+            is = clientSocket.getInputStream();
+        } catch (IOException ex) {
+            // Do exception handling
+        }
 
-                pwr.flush();
-            }
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
 
-        // receive file
-        @SuppressWarnings("resource")
-        ZipFile file = new ZipFile(FILE_TO_RECEIVED) ; 
-        System.out.println(file.size()+ "in client"); 
-        System.out.println("File " + FILE_TO_RECEIVED  +" has no of files "+file.size() + " downloaded (" + current + " bytes read)");
-    } catch (IOException e) {
-        System.out.println("Exception in Input Stream in Client");
-        e.printStackTrace();
-    }
-    finally {
-        if (fos != null){
+        if (is != null) {
+
+            FileOutputStream fos = null;
+            BufferedOutputStream bos = null;
             try {
-                fos.close();
-                if (bos != null) bos.close();
-                if (sock != null) sock.close();
-            } catch (IOException e) {
-                System.out.println("Exception in closing FileOutputStream or BufferedReader or Socket");
-                e.printStackTrace();
+                fos = new FileOutputStream( fileOutput );
+                bos = new BufferedOutputStream(fos);
+                bytesRead = is.read(aByte, 0, aByte.length);
+                
+                //read bytewise from connection
+                do {
+                        baos.write(aByte);
+                        bytesRead = is.read(aByte);
+                } while (bytesRead != -1);
+
+                bos.write(baos.toByteArray());
+                bos.flush();
+                bos.close();
+                clientSocket.close();
+            } catch (IOException ex) {
+                // Do exception handling
             }
         }
-    }
-}
-public static Socket connectToServer(String server2, int socketPort) {
-    Socket sock = null;
-    try {
-        sock = new Socket(server2, socketPort);
-    } catch (IOException e) {
-        System.out.println("Exception in establishing connection with server");
-        e.printStackTrace();
-    }
-    return sock;
     }
 }
--- ZipTransfer/src/transfer/TCPServer.java
+++ ZipTransfer/src/transfer/TCPServer.java
@@ -1,71 +1,53 @@
 package transfer;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.PrintWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.io.*;
+import java.net.*;
 
-public class TCPServer 
-{
-    public final static int SOCKET_PORT = 13267;  
-    public final static String FILE_TO_SEND = "C:/Public/Pictures/Sample Pictures.zip";  
-    public static void main (String [] args ) throws IOException {
-    FileInputStream fis;
-    BufferedInputStream bis = null; 
-    BufferedReader br;
-    OutputStream os = null;
-    ServerSocket servsock = null;
-    PrintWriter pw;
-    Socket sock = null;
+class TCPServer {
 
-    try {
-        servsock = new ServerSocket(SOCKET_PORT);
+    private final static String fileToSend = "/home/donald/git/eva18/ZipTransfer/AntragGEZ.pdf";
+
+    public static void main(String args[]) {
+
         while (true) {
-            System.out.println("Waiting...");
+            ServerSocket welcomeSocket = null;
+            Socket connectionSocket = null;
+            BufferedOutputStream outToClient = null;
+
             try {
-                sock = servsock.accept();
-                System.out.println("Accepted connection : " + sock);  
-                pw = new PrintWriter(sock.getOutputStream(), true);
-                pw.println("SendZip");
-                br = new BufferedReader(new InputStreamReader(sock.getInputStream()));
-                File myFile = new File (FILE_TO_SEND);
-                Integer i = (int) (long) myFile.getTotalSpace();
-                String input, output;
-                while ((input = br.readLine()) != null) {
-                     System.out.println("in while loop");
-                    if(input.equals("SENDZIP"))
-                     {
-                         output = "SENTZIP";
-                         pw.println(output);
-                         System.out.println(input+ " is the input and output is "+output);
-                         byte [] mybytearray  = new byte [(int)myFile.length()];
-                         fis = new FileInputStream(myFile);
-                         bis = new BufferedInputStream(fis);
-                         bis.read(mybytearray,0,mybytearray.length);
-                         os = sock.getOutputStream();
-                         System.out.println("Sending " + FILE_TO_SEND + "(" + mybytearray.length + " bytes)");
-                         os.write(mybytearray,0,mybytearray.length);
-                         os.flush(); 
-                        }
-                     }
-                    pw.flush();
-            System.out.println("Done.");
+                welcomeSocket = new ServerSocket(3248);
+                connectionSocket = welcomeSocket.accept();
+                outToClient = new BufferedOutputStream(connectionSocket.getOutputStream());
+            } catch (IOException ex) {
+            	ex.printStackTrace();
             }
-            finally {
-                if (bis != null) bis.close();
-                if (os != null) os.close();
-                if (sock!=null) sock.close();
+
+            if (outToClient != null) {
+                File myFile = new File( fileToSend );
+                byte[] mybytearray = new byte[(int) myFile.length()];
+                System.out.println("Filelength: "+myFile.length());
+                FileInputStream fis = null;
+
+                try {
+                    fis = new FileInputStream(myFile);
+                } catch (FileNotFoundException ex) {
+                	ex.printStackTrace();
+                }
+                BufferedInputStream bis = new BufferedInputStream(fis);
+
+                try {
+                    bis.read(mybytearray, 0, mybytearray.length);
+                    outToClient.write(mybytearray, 0, mybytearray.length);
+                    outToClient.flush();
+                    outToClient.close();
+                    connectionSocket.close();
+
+                    // File sent, exit the main method
+                    return;
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
             }
         }
     }
-    finally {
-        if (servsock != null) servsock.close();
-        }
-    }
- }
+}
